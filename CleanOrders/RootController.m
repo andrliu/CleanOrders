2d4e385faf02e0164e4abcd6864c9c278789d5b9
andrliu <dieseliu321@gmail.com>
2015-06-12 19:11:44 -0700

    just started

---
+++ CleanOrders/RootController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  CleanOrders
+//
+//  Created by Andrew Liu on 6/12/15.
+//  Copyright (c) 2015 Andrew Liu. All rights reserved.
+//
+
+#import "RootController.h"
+
+@interface RootController ()
+
+@end
+
+@implementation RootController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+@end
